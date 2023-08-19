c6715b346b2b83cc255714f2e98b3169b4f5a572
tusooa <tusooa@kazv.moe>
2023-08-18 21:19:41 -0400

    Show unread marker for extra notifications

--- src/components/extra_notifications/extra_notifications.vue
+++ src/components/extra_notifications/extra_notifications.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="ExtraNotifications">
-    <router-link
-      v-if="shouldShowChats"
-      class="button-unstyled -link extra-notification"
-      :to="{ name: 'chats', params: { username: currentUser.screen_name } }"
-    >
-      {{ $tc('notifications.unread_chats', unreadChatCount, { num: unreadChatCount }) }}
-    </router-link>
-    <router-link
-      v-if="shouldShowAnnouncements"
-      class="button-unstyled -link extra-notification"
-      :to="{ name: 'announcements' }"
-    >
-      {{ $tc('notifications.unread_announcements', unreadAnnouncementCount, { num: unreadAnnouncementCount }) }}
-    </router-link>
-    <router-link
-      v-if="shouldShowFollowRequests"
-      class="button-unstyled -link extra-notification"
-      :to="{ name: 'friend-requests' }"
-    >
-      {{ $tc('notifications.unread_follow_requests', followRequestCount, { num: followRequestCount }) }}
-    </router-link>
+    <div class="notification unseen">
+      <div class="notification-overlay"></div>
+      <router-link
+        v-if="shouldShowChats"
+        class="button-unstyled -link extra-notification-link"
+        :to="{ name: 'chats', params: { username: currentUser.screen_name } }"
+      >
+        {{ $tc('notifications.unread_chats', unreadChatCount, { num: unreadChatCount }) }}
+      </router-link>
+    </div>
+    <div class="notification unseen">
+      <div class="notification-overlay"></div>
+      <router-link
+        v-if="shouldShowAnnouncements"
+        class="button-unstyled -link extra-notification-link"
+        :to="{ name: 'announcements' }"
+      >
+        {{ $tc('notifications.unread_announcements', unreadAnnouncementCount, { num: unreadAnnouncementCount }) }}
+      </router-link>
+    </div>
+    <div class="notification unseen">
+      <div class="notification-overlay"></div>
+      <router-link
+        v-if="shouldShowFollowRequests"
+        class="button-unstyled -link extra-notification-link"
+        :to="{ name: 'friend-requests' }"
+      >
+        {{ $tc('notifications.unread_follow_requests', followRequestCount, { num: followRequestCount }) }}
+      </router-link>
+    </div>
     <i18n-t
       v-if="shouldShowCustomizationTip"
       tag="span"
-      class="extra-notification tip"
+      class="notification tip"
       keypath="notifications.configuration_tip"
     >
       <template #theSettings>
@@ -58,12 +67,9 @@
   flex-direction: column;
   align-items: stretch;
 
-  .extra-notification {
+  .notification {
     width: 100%;
     padding: 1em;
-  }
-
-  .extra-notification {
     border-bottom: 1px solid;
     border-color: $fallback--border;
     border-color: var(--border, $fallback--border);
